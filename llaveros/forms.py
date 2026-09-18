4d5f558f02f6814000c7be48ce27ea7b4d16332c
Cristian Prieto <pcristiandev@gmail.com>
2026-09-18 12:06:26 -0300

    Agregar funcionalidades de crear, listar, actualizar mascotas

--- llaveros/forms.py
+++ llaveros/forms.py
@@ -1,7 +1,13 @@
 from django import forms
-from .models import Usuario
+from .models import Usuario, Mascota
 
 class UsuarioForm(forms.ModelForm):
     class Meta:
         model = Usuario
         fields = ("nombre", "apellido", "email", "telefono")
+
+
+class MascotaForm(forms.ModelForm):
+    class Meta:
+        model = Mascota
+        fields = ("nombre", "estado", "imagen", "usuario")
