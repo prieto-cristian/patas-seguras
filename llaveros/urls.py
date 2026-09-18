eb9e522a980ae5f9d2b17038ce3344ff4be05c50
Cristian Prieto <pcristiandev@gmail.com>
2026-09-18 11:10:46 -0300

    Agregar funcionalidades para listar, crear, actualizar usuarios

--- llaveros/urls.py
+++ llaveros/urls.py
@@ -1,6 +1,10 @@
 from django.urls import path
-from .views import inicio
+from .views import (inicio, UsuarioListView, UsuarioCreateView,
+                    UsuarioUpdateView)
 
 urlpatterns = [
-    path("", inicio),
+    path("", inicio, name='inicio'),
+    path("usuarios/", UsuarioListView.as_view(), name="listar_usuarios"),
+    path("usuarios/crear/", UsuarioCreateView.as_view(), name="crear_usuario"),
+    path('usuarios/modificar/<pk>/', UsuarioUpdateView.as_view(), name="modificar_usuario"),
 ]
